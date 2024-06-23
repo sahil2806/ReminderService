a671deb1d48626bdde315f22de950642cc3fb63f
sahil2806 <sahilsahu2806@gmail.com>
2024-06-23 07:26:36 +0530

    Added NotificationTicket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
-
+var cron = require('node-cron');
 const {sendBasicEmail} = require('./service/email-service')
 
 const setupAndStartServer = () => {
@@ -12,12 +12,15 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, async () =>{
         console.log(`Server started at port ${PORT}`);
-        await sendBasicEmail(
-            'sahusahil2226@gmail.com',
-            'bt22cse101@iiitn.ac.in',
-            'Testing message',
-            'How are you'
-        );
+        // await sendBasicEmail(
+        //     'sahusahil2226@gmail.com',// sender
+        //     'bt22cse101@iiitn.ac.in',// receiver
+        //     'Testing message', // title
+        //     'How are you' // body
+        // );
+        // cron.schedule('* * * * *', () => {
+        //     console.log('running a task every minute');
+        // });
         
     })
 }
