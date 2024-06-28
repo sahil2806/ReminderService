321ffab8ce48a5327ac42fd3d72ff6b434d71c15
sahil2806 <sahilsahu2806@gmail.com>
2024-06-28 18:04:07 +0530

    V1 completed

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const setupAndStartServer = async  () => {
     const app = express();
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
+    
     app.post('/api/v1/tickets', TicketController.create);
 
     const channel = await createChannel();
